bf0ec4145769899843a8529e9b8aa2d913347f80
yuwki0131 <yuwkixxx@gmail.com>
2021-09-06 00:41:36 +0900

    fix errors

--- init.el
+++ init.el
@@ -6,14 +6,14 @@
 ;; WIP: 設定ファイルのルート
 (add-to-list 'load-path "~/.emacs.d/alter")
 
+(require 'min-packages)
 (require 'builtin)
 (require 'font)
 (require 'color)
 (require 'appearance)
 (require 'key)
-(require 'min-packages)
 
-(setq default-directory "~/")
+'(setq default-directory "~/")
 
 ;;; position
 
@@ -23,23 +23,5 @@
         (width . 100)
         (height . 55)
         ))
-
-(setq-default mode-line-format
-              '((:eval
-                 (cond
-                   (buffer-read-only
-                    (propertize " ⚑ "
-                                'face '(:foreground "red" :weight 'bold)
-                                'help-echo "buffer is read-only!!!"))
-                   ((buffer-modified-p)
-                    (propertize " ☡ "
-                                'face '(:foreground "orange")
-                                'help-echo "buffer modified."))))
-                (:eval (if (memq (buffer-local-value 'major-mode (current-buffer))
-                                 '(ruby-mode enh-ruby-mode))
-                           (propertize "Ruby" 'face '(:foreground "green"))
-                         (propertize "Not Ruby" 'face '(:foreground "orange"))))))
-
-(setq frame-title-format "full load")
 
 ;;; init.el ends here
