a84d787e684c8282ea17c9130580122a5571adf5
yuwki0131 <yuwkixxx@gmail.com>
2025-06-07 19:47:36 +0900

    Guard font configuration functions

--- alter/font.el
+++ alter/font.el
@@ -12,20 +12,27 @@
 (defvar default-font-jp-name "HackGen35 Console")
 
 ;; 半角フォント: abc...
-(set-face-attribute
- 'default nil :family default-font-name :height 110)
+(when (fboundp 'set-face-attribute)
+  (set-face-attribute
+   'default nil :family default-font-name :height 110))
 
 ;; 日本語全角フォント: あいうえお...
-(set-fontset-font
- 'nil 'japanese-jisx0208 (font-spec :family default-font-jp-name :height 105))
+(when (fboundp 'set-fontset-font)
+  (set-fontset-font
+   'nil 'japanese-jisx0208
+   (font-spec :family default-font-jp-name :height 105)))
 
 ;; ギリシャ文字全角フォント: αβγκλ...
-(set-fontset-font
- 'nil '(#x0370 . #x03FF) (font-spec :family default-font-name :height 120))
+(when (fboundp 'set-fontset-font)
+  (set-fontset-font
+   'nil '(#x0370 . #x03FF)
+   (font-spec :family default-font-name :height 120)))
 
 ;; キリル文字全角フォント: Эта статья ... Русский
-(set-fontset-font
- 'nil '(#x0400 . #x04FF) (font-spec :family default-font-name :height 120))
+(when (fboundp 'set-fontset-font)
+  (set-fontset-font
+   'nil '(#x0400 . #x04FF)
+   (font-spec :family default-font-name :height 120)))
 
 ;;;---------------------------------------------------------------------------
 ;;; provide
